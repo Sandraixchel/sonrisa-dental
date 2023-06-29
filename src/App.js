bb5d862bce77a7fde64579b1057c0d40426bc641
Sandra Ixchel López Acosta <115147887+Sandraixchel@users.noreply.github.com>
2023-06-30 00:32:32 +0100

    Routing Config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./layout/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddUser from "./users/AddUser";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/adduser" element={<AddUser />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
